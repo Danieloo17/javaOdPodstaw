c509819c8c877936da050763cfe731d07f78e8f9
Danieloo17 <156961875+danieloo17@users.noreply.github.com>
2024-12-21 11:43:39 +0100

    Study

---
+++ study/src/exampless/firstStep.java
@@ -0,0 +1,14 @@
+package exampless;
+
+public class firstStep {
+    public static void main(String[] args) {
+        String name = "Daniel";
+        int age = 30;
+
+        System.out.println("My name is " + name + " and I'm " + age + " old!");
+
+    }
+
+
+
+}
