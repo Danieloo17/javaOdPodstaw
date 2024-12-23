d194bd9ab09c2e6b4556ddcdb85730b52f7e4f24
Daniel Nowinski <daniel.nowinski.84@gmail.com>
2024-12-23 23:18:18 +0100

    Study

--- study/src/exampless/floatingPointNumbers.java
+++ study/src/exampless/floatingPointNumbers.java
@@ -1,7 +1,20 @@
 package exampless;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class floatingPointNumbers {
     public static void main(String[] args) {
+        double a = 2.0;
+        double b = 1.5;
+        double c = a / b;
+
+        System.out.println(Math.round(c*100)/100.0);
+        BigDecimal roundedNumber = new BigDecimal(c).setScale(2, RoundingMode.HALF_UP);
+        System.out.println(roundedNumber.doubleValue());
+        String formatted = String.format("%.2f", c);
+        System.out.println(formatted);
+
 
     }
 }
