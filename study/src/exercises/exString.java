6958a1a440b0b805ba14fded0f64b38f365bda58
Daniel Nowinski <daniel.nowinski.84@gmail.com>
2024-12-29 12:25:46 +0100

    Study

--- study/src/exercises/exString.java
+++ study/src/exercises/exString.java
@@ -3,9 +3,9 @@ package exercises;
 public class exString {
     public static void main(String[] args) {
         String kurs = "  Jak nauczyć się programowania   ";
-        kurs = kurs.strip();
-        kurs = kurs.toUpperCase();
-        kurs = kurs.substring(4);
+        kurs = kurs.strip().toUpperCase().substring(4);
+//        kurs = kurs.toUpperCase();
+//        kurs = kurs.substring(4);
         System.out.println(kurs);
     }
 }
